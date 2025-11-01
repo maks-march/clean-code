78dc1e13d3c8fe29257632527ab2ea9f60e4059f
marchenko <1maks_2055@mail.ru>
2025-11-01 22:25:44 +0500

    Tokenizer is static and renamed

--- cs/Markdown/Tokeniezer.cs
+++ cs/Markdown/Tokeniezer.cs
@@ -1,18 +1,18 @@
 namespace Markdown
 {
-    public class Tokeniezer
-    {
-        public Token[] GetTokens(string text)
+    public static class Tokeniezer
+    {        
+        public static Token[] GetTokens(string text)
         {
             throw new Exception();
         }
 
-        private IEnumerable<string> ParseText(string text)
+        private static IEnumerable<string> ParseText(string text)
         {
             throw new Exception();
         }
         
-        private Token[] TokeniezeLine(string line)
+        private static Token[] TokeniezeLine(string line)
         {
             throw new Exception();
         }
